9be1af47d69eaa1ac5149a5b602c48cb331cd30f
Albert Bannister <albibannister@gmail.com>
2026-08-21 17:07:07 +1200

    Switch from float to decimal and fix lower brackets

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,28 +1,30 @@
 from dataclasses import dataclass
+from typing import Any
 from loguru import logger
+from decimal import Decimal
 
 
 @dataclass
 class PAYETaxBracket:
     "Class representing an income tax bracket"
 
-    lower: float
-    upper: float
-    tax_rate: float
+    lower: Decimal
+    upper: Decimal
+    tax_rate: Decimal
 
 
 TAX_BRACKETS = [
-    PAYETaxBracket(0.0, 15600.0, 10.5),
-    PAYETaxBracket(15601.0, 53500.0, 17.5),
-    PAYETaxBracket(53500.0, 78100.0, 30.0),
-    PAYETaxBracket(78101.0, 180000.0, 33.0),
-    PAYETaxBracket(180000.0, float("inf"), 39.0),
+    PAYETaxBracket(Decimal(0), Decimal(15600), Decimal(10.5)),
+    PAYETaxBracket(Decimal(15600.0), Decimal(53500.0), Decimal(17.5)),
+    PAYETaxBracket(Decimal(53500.0), Decimal(78100.0), Decimal(30.0)),
+    PAYETaxBracket(Decimal(78100.0), Decimal(180000.0), Decimal(33.0)),
+    PAYETaxBracket(Decimal(180000.0), Decimal("Infinity"), Decimal(39.0)),
 ]
 
 
-def calculate_tax(gross_income: float) -> float:
+def calculate_tax(gross_income: Decimal) -> Decimal:
     logger.info(f"Calculating PAYE tax for {gross_income}")
-    total_paye_tax: float = 0.0
+    total_paye_tax: Decimal = Decimal(0.0)
     for tax_bracket in TAX_BRACKETS:
         if gross_income < tax_bracket.lower:
             break
@@ -31,7 +33,7 @@ def calculate_tax(gross_income: float) -> float:
             min(tax_bracket.upper, gross_income) - tax_bracket.lower
         )
         logger.debug(f"Taxable income in bracket: {taxable_income_in_bracket}")
-        calculated_paye_in_bracket = (taxable_income_in_bracket * tax_bracket.tax_rate) / 100.0
+        calculated_paye_in_bracket = (taxable_income_in_bracket * tax_bracket.tax_rate) / Decimal(100.0)
         logger.debug(f"Calculated PAYE: {calculated_paye_in_bracket}")
         total_paye_tax += calculated_paye_in_bracket
 
--- test_tax_calculator.py
+++ test_tax_calculator.py
@@ -1,15 +1,16 @@
 import pytest
 from tax_calculator import calculate_tax
+from decimal import Decimal
 
-TEST_CASES: list[tuple[float, float]] = [
-    (10000.00, 1050),
-    (35000.00, 5033.00),
-    (100000.00, 22877.50),
-    (220000.00, 64877.50),
+TEST_CASES: list[tuple[Decimal, Decimal]] = [
+    (Decimal(10000.00), Decimal(1050)),
+    (Decimal(35000.00), Decimal(5033.00)),
+    (Decimal(100000.00), Decimal(22877.50)),
+    (Decimal(220000.00), Decimal(64877.50)),
 ]
 
 
 @pytest.mark.parametrize("test_case", TEST_CASES)
-def test_calculate_tax(test_case: tuple[float, float]):
+def test_calculate_tax(test_case: tuple[Decimal, Decimal]):
     gross_income, expected_tax = test_case
     assert calculate_tax(gross_income) == expected_tax
