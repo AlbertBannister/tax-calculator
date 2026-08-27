54b3ad81e9f453aa22b9f65f28feed88f83eb918
Albert Bannister <albibannister@gmail.com>
2026-08-28 10:11:12 +1200

    Checkout

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.get("/calculate/paye")
 def calculate_paye():
     raw_gross_income = request.args.get("gross_income", "")
-    parsed_gross_income = PositiveCurrency(Decimal(raw_gross_income))
+    parsed_gross_income = PositiveCurrency.from_any(raw_gross_income)
     calculated_paye = calculate_tax(parsed_gross_income)
 
     return {"paye_tax": calculated_paye}
